2df60a5b13b9c49ceb32b6227a101cca1cc0d6d7
TheFastFalcons <149209132+TheFastFalcons@users.noreply.github.com>
2023-11-17 01:58:47 +0300

    deploy 2

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/my-spa-rick/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/rick_and_morty_spa_vue_3/' : '/',
   transpileDependencies: true
 }
